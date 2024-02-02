826bb4002ac7b9b3949d6329d490add576b16941
Shawna Metzger <32371888+MetzgerSK@users.noreply.github.com>
2024-02-02 09:50:50 -0500

    Dealing with `testthat` issue on Ubuntu 22.04
    Binder updated their base image to 22.04 in OCT23 due to EOL on 18.04, and 22.04 puts the issue described here in play: https://www.github.com/r-lib/testthat/issues/1373.

--- .binder/install.R
+++ .binder/install.R
@@ -10,7 +10,6 @@ install.packages("dplyr")
 install.packages("forecast")
 install.packages("foreign")
 install.packages("future")
-install.packages("ggplot2") 
 install.packages("glmnet")
 install.packages("glue")
 install.packages("haven")
@@ -47,4 +46,16 @@ install.packages("stringr")
 install.packages("survival")
 install.packages("tibble")
 install.packages("zoo")
+
+
+# Reinstall anything involving ggplot
+remotes::install_version("testthat", "3.2.1", upgrade="never", repos="http://cran.us.r-project.org") # b/c older testthats run into problems on Ubuntu 22.04, which Binder now uses as of OCT23, and several packages will try to install testthat.  Without this fix, those installs will fail.
+install.packages("ggplot2") 
 remotes::install_github("wilkelab/ggtext", ref = "50fdaba101b2c591c08e9d67782a5b86621a4fa9")    # b/c CRAN build not avail for 3.6.3
+install.packages("qqplotr")
+install.packages("plotly")
+install.packages("forecast")
+
+
+# Force dplyr to be 0.8.4, as it would have likely been on 2020-04-06 (somewhere along the way, after all the above finishes, 1.1.4 is getting installed)
+remotes::install_version("dplyr", "0.8.4") 
